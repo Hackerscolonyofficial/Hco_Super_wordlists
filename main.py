3254c20f3383f37ad98ad5e9426e473c19af0ba8
Hackers Colony Official <103638018+Hackerscolonyofficial@users.noreply.github.com>
2025-02-09 17:31:48 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from os import system 
+from files.URLS import whatsapp,instagram,youtube 
+from files.Hadings import Heading, Sub_head
+from files.Writer import writer 
+
+
+Heading()
+print('\n'*2)
+text = '''                This tool for create wordlists
+                This tool is very powerful. 
+                this tool for only education purpose. 
+                so please don'tuse for illegal work'''
+
+writer(text)
+
+youtube()
+input('Tap Enter:  ')
+instagram()
+input('Tap Enter:  ')
+whatsapp()
+
+system('clear')
+Sub_hading()
+print('\n'*2)
+system('python files/super_wordlists.py')
